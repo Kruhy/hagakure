86c185cf373f30860348cf5ab02823f32383831d
Piotr Szczygielski <piotrszczygielski@gmail.com>
2021-01-05 20:08:02 +0100

    initial commit

---
+++ hagakure/local_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'mt&+q9s5s)jk@0kw#_wb7q0#)n7mfa=fq#i^h!qc5#2ie2!h4j'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'charity_donation_db',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
