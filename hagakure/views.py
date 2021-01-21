970f2b3b03acd89d2f36681dc8d70d6450d11fe1
Piotr <piotrszczygielski@gmail.com>
2021-01-21 09:28:59 +0100

    minor tweeks to templates

--- hagakure/views.py
+++ hagakure/views.py
@@ -5,59 +5,62 @@ from django.views import View
 
 from utils.functions import display_name
 
-from trainings.models import Training
+from trainings.models import Training, Weekday
 
 User = get_user_model()
 
 class LandingPageView(View):
     def get(self, request, *args, **kwargs):
         week_beggining_date = date.today() - timedelta(days=date.today().weekday())
-        
-        monday_trainings = Training.objects.filter(
-            weekdays=1,
+
+        weekdays = Weekday.objects.all().order_by('order')
+
+        day1_trainings = Training.objects.filter(
+            weekdays__order=1,
             start_date__lte=week_beggining_date + timedelta(days=0),
             end_date__gte=week_beggining_date + timedelta(days=0),
             ).order_by('start_hour')
-        tuesday_trainings = Training.objects.filter(
-            weekdays=2,
+        day2_trainings = Training.objects.filter(
+            weekdays__order=2,
             start_date__lte=week_beggining_date + timedelta(days=1),
             end_date__gte=week_beggining_date + timedelta(days=1),
             ).order_by('start_hour')
-        wednesday_trainings = Training.objects.filter(
-            weekdays=3,
+        day3_trainings = Training.objects.filter(
+            weekdays__order=3,
             start_date__lte=week_beggining_date + timedelta(days=2),
             end_date__gte=week_beggining_date + timedelta(days=2),
             ).order_by('start_hour')
-        thursday_trainings = Training.objects.filter(
-            weekdays=4,
+        day4_trainings = Training.objects.filter(
+            weekdays__order=4,
             start_date__lte=week_beggining_date + timedelta(days=3),
             end_date__gte=week_beggining_date + timedelta(days=3),
             ).order_by('start_hour')
-        friday_trainings = Training.objects.filter(
-            weekdays=5,
+        day5_trainings = Training.objects.filter(
+            weekdays__order=5,
             start_date__lte=week_beggining_date + timedelta(days=4),
             end_date__gte=week_beggining_date + timedelta(days=4),
             ).order_by('start_hour')
-        saturday_trainings = Training.objects.filter(
-            weekdays=6,
+        day6_trainings = Training.objects.filter(
+            weekdays__order=6,
             start_date__lte=week_beggining_date + timedelta(days=5),
             end_date__gte=week_beggining_date + timedelta(days=5),
             ).order_by('start_hour')
-        sunday_trainings = Training.objects.filter(
-            weekdays=7,
+        day7_trainings = Training.objects.filter(
+            weekdays__order=7,
             start_date__lte=week_beggining_date + timedelta(days=6),
             end_date__gte=week_beggining_date + timedelta(days=6),
             ).order_by('start_hour')
 
         context = {
             'name': display_name(request.user),
-            'monday_trainings': monday_trainings,
-            'tuesday_trainings': tuesday_trainings,
-            'wednesday_trainings': wednesday_trainings,
-            'thursday_trainings': thursday_trainings,
-            'friday_trainings': friday_trainings,
-            'saturday_trainings': saturday_trainings,
-            'sunday_trainings': sunday_trainings,
+            'day1_trainings': day1_trainings,
+            'day2_trainings': day2_trainings,
+            'day3_trainings': day3_trainings,
+            'day4_trainings': day4_trainings,
+            'day5_trainings': day5_trainings,
+            'day6_trainings': day6_trainings,
+            'day7_trainings': day7_trainings,
+            'weekdays': weekdays,
         }
         return render(request, 'hagakure/index.html', context)
 
